34b1e4a5794a6dec6a77c662c28abf7f09c10a0a
Diogo Nunes <diogoabnunes@gmail.com>
2020-04-03 15:27:30 +0100

    tp shortest path done

---
+++ TP/cal_fp05_CLion/cmake-build-debug/lib/googletest-master/googlemock/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/diogo/Documentos/CAL/TP/cal_fp05_CLion/lib/googletest-master/googletest
+# Build directory: /home/diogo/Documentos/CAL/TP/cal_fp05_CLion/cmake-build-debug/lib/googletest-master/googlemock/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
